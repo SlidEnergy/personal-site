ab5b4ed320a6287660e898765e56d40eaa076f3b
slidenergy <slidenergy@gmail.com>
2018-12-04 20:34:46 +1000

    Переписал профессиональные навыки с таблица на списки

--- src/app/about-author/about-me-formal/prof-skills/prof-skills.component.ts
+++ src/app/about-author/about-me-formal/prof-skills/prof-skills.component.ts
@@ -8,46 +8,6 @@ import { untilComponentDestroyed } from '@w11k/ngx-componentdestroyed';
   styleUrls: ['./prof-skills.component.scss']
 })
 export class ProfSkillsComponent implements OnInit, OnDestroy {
-
-  progLang = {
-    nameRu: 'Языки и разметка',
-    nameEn: 'Languages and markup',
-    beginner: 'PowerShell',
-    intermediate: 'JavaScript, TypeScript, T-SQL, HTML, CSS',
-    expert: 'C#'
-  };
-
-  technology = {
-    nameRu: 'Технологии',
-    nameEn: 'Technologies',
-    beginner: 'NodeJS, Entity Framework',
-    intermediate: 'NUnit testing, sockets',
-    expert: 'WinForms, ado.net, ASP.NET, WebApi, async programming'
-  };
-
-  visualComponents = {
-    nameRu: 'Визуальные компоненты',
-    nameEn: 'Visual components',
-    beginner: '',
-    intermediate: '',
-    expert: 'DevExpress WinForms/ASP.NET/JS&HTML'
-  };
-
-  frameworks = {
-    nameRu: 'Фреймворки',
-    nameEn: 'Frameworks',
-    beginner: 'Bootstrap, AngularJS',
-    intermediate: 'Angular 2+',
-    expert: '.NET 4.6'
-  };
-
-  items = [
-    this.progLang,
-    this.technology,
-    this.visualComponents,
-    this.frameworks,
-  ];
-
   currentLang = this.translate.currentLang;
 
   constructor(private translate: TranslateService) {
