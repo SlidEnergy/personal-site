fee760b99b371c6672ff96201b0d332df760a48f
slidenergy <slidenergy@gmail.com>
2022-10-12 23:21:11 +0600

    update angular cli to 12

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -58,5 +58,5 @@ import * as process from 'process';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone'; // Included with Angular CLI.
+import 'zone.js'; // Included with Angular CLI.
 window['process'] = process;
